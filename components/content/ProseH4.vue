bacaf2ddb73d0e5344362470720e6b48ae239168
enpitsulin <enpitsulin@gmail.com>
2024-05-19 20:50:54 +0800

    refactor: to split components

--- components/content/ProseH4.vue
+++ components/content/ProseH4.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+defineProps<{ id?: string }>()
+</script>
+
 <template>
-  <Heading :level="5">
-    <slot />
-  </Heading>
+  <h4 :id="id" class="group flex items-center">
+    <span>
+      <slot />
+    </span>
+    <a
+      flex="inline"
+      op="0 group-hover:100 focus:100"
+      class="transition-opacity duration-200"
+      role="button"
+      tabindex="0"
+      aria-hidden="true"
+      ml-2 select-none text-accent
+      :href="`#${id}`"
+    >
+      <i class="i-mingcute:hashtag-line" size-1em />
+    </a>
+  </h4>
 </template>
