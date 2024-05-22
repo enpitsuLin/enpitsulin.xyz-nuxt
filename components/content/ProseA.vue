6b1b6c3916d8746567f7fd653a7bf3073a43530a
enpitsulin <enpitsulin@gmail.com>
2024-05-22 21:03:31 +0800

    feat: custom only external link

--- components/content/ProseA.vue
+++ components/content/ProseA.vue
@@ -1,16 +1,37 @@
 <script setup lang="ts">
+import type { AnchorHTMLAttributes } from 'vue'
 
+defineOptions({
+  inheritAttrs: false,
+})
+
+const attrs = useAttrs() as AnchorHTMLAttributes
+
+function isExternalLink(url?: string) {
+  const match = url?.match(/http[s?]:\/\//)
+  if (!match)
+    return false
+
+  return match
+}
 </script>
 
 <template>
-  <span role="note" flex="inline items-center">
-    <a
-      v-bind="$attrs" decoration-none
-      class="from-accent to-accent duration-250 transition-property-[background-size] hover:bg-[size:100%_2px]"
-      bg="gradient-to-r no-repeat [position:left_bottom] [size:0_2px] "
-    >
-      <ContentSlot :use="$slots.default" />
+  <template v-if="isExternalLink(attrs.href)">
+    <span role="note" flex="inline items-center">
+      <a
+        :="attrs" decoration-none
+        class="from-accent to-accent duration-250 transition-property-[background-size] hover:bg-[size:100%_2px]"
+        bg="gradient-to-r no-repeat [position:left_bottom] [size:0_2px] "
+      >
+        <ContentSlot :use="$slots.default" />
+      </a>
+      <i class="i-mingcute:arrow-right-up-line translate-y-2px op-70" inline-block />
+    </span>
+  </template>
+  <template v-else>
+    <a :="attrs" decoration-none>
+      <slot />
     </a>
-    <i class="i-mingcute:arrow-right-up-line translate-y-2px op-70" inline-block />
-  </span>
+  </template>
 </template>
