260ce3443790cefb7aa84171d60ff3ce8288ae44
enpitsulin <enpitsulin@gmail.com>
2024-05-24 15:24:26 +0800

    feat: extract to utils

--- server/plugins/0.content.ts
+++ server/plugins/0.content.ts
@@ -1,5 +1,5 @@
-import type { MarkdownNode, ParsedContent } from '@nuxt/content/dist/runtime/types'
-import { SKIP, visit } from 'unist-util-visit'
+import type { ParsedContent } from '@nuxt/content/dist/runtime/types'
+import { parseLinkNode, unwrapElement } from '../utils/content'
 
 declare module 'nitropack' {
   export interface NitroRuntimeHooks {
@@ -7,42 +7,12 @@ declare module 'nitropack' {
     'content:file:afterParse': (file: ParsedContent) => void
   }
 }
-const GITHUB_HOST_REG = /https:\/\/github.com\/\S+\/\S+/
-function isGithubRepoUrl(url: string) {
-  return url.match(GITHUB_HOST_REG)
-}
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('content:file:afterParse', (file) => {
     if (file._id.endsWith('.md')) {
-      visit(
-        file.body!,
-        (node): node is MarkdownNode => 'tag' in node && node.tag === 'a',
-        (node, _index, parent) => {
-          if (!node.props)
-            return
-          if (isGithubRepoUrl(node.props.href) && parent?.children.length === 1)
-            node.tag = 'prose-github-card'
-        },
-      )
-
-      visit(
-        file.body!,
-        (node): node is MarkdownNode => {
-          return 'tag' in node && node.tag === 'p'
-            && 'children' in node && Array.isArray(node.children)
-            && node.children.length === 1
-            && node.children.at(0).type === 'element'
-            && node.children.at(0).tag === 'prose-github-card'
-        },
-        (node, index, parent) => {
-          if (!parent || typeof index === 'undefined' || !node.children)
-            return false
-
-          parent.children.splice(index, 1, ...node.children)
-          return [SKIP, index]
-        },
-      )
+      parseLinkNode(file.body!)
+      unwrapElement(file.body!)
     }
   })
 })
---
+++ server/utils/content.ts
@@ -0,0 +1,40 @@
+import type { MarkdownNode, MarkdownRoot } from '@nuxt/content/types'
+import { SKIP, visit } from 'unist-util-visit'
+
+const GITHUB_HOST_REG = /https:\/\/github.com\/\S+\/\S+/
+function isGithubRepoUrl(url: string) {
+  return url.match(GITHUB_HOST_REG)
+}
+
+export function parseLinkNode(node: MarkdownNode | MarkdownRoot) {
+  visit(
+    node,
+    (node): node is MarkdownNode => 'tag' in node && node.tag === 'a',
+    (node, _index, parent) => {
+      if (!node.props)
+        return
+      if (isGithubRepoUrl(node.props.href) && parent?.children.length === 1)
+        node.tag = 'prose-github-card'
+    },
+  )
+}
+
+export function unwrapElement(node: MarkdownNode | MarkdownRoot) {
+  visit(
+    node,
+    (node): node is MarkdownNode => {
+      return 'tag' in node && node.tag === 'p'
+        && 'children' in node && Array.isArray(node.children)
+        && node.children.length === 1
+        && node.children.at(0).type === 'element'
+        && node.children.at(0).tag === 'prose-github-card'
+    },
+    (node, index, parent) => {
+      if (!parent || typeof index === 'undefined' || !node.children)
+        return false
+
+      parent.children.splice(index, 1, ...node.children)
+      return [SKIP, index] as const
+    },
+  )
+}
