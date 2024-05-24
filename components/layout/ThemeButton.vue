0066b9430a5bca844444770ca857082c442151e4
enpitsulin <enpitsulin@gmail.com>
2024-05-25 00:47:55 +0800

    refactor: fab buttons

---
+++ components/layout/ThemeButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+function onClick() {
+  colorMode.preference = colorMode.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <button
+    type="button"
+    flex="~ items-center justify-center" of-hidden
+    class="size-8 bg-opacity-80 transition-all active:scale-105 hover:scale-115"
+    @click="onClick"
+  >
+    <span class="sr-only">change dark mode</span>
+    <ClientOnly>
+      <i v-if="colorMode.preference === 'dark'" inline-block class="i-mingcute:moon-line" />
+      <i v-else-if="colorMode.preference === 'light'" inline-block class="i-mingcute:sun-line" />
+      <i v-else inline-block class="i-mingcute:computer-line" />
+      <template #fallback>
+        <i inline-block class="i-mingcute:computer-line" />
+      </template>
+    </ClientOnly>
+  </button>
+</template>
---
+++ components/layout/TopUpButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { y } = useWindowScroll()
+</script>
+
+<template>
+  <Transition
+    enter-active-class="transition-property-[height,opacity] duration-300 ease-in-out"
+    leave-active-class="transition-property-[height,opacity] duration-300 ease-in-out"
+    enter-from-class="h-0 op-0"
+    leave-to-class="h-0 op-0"
+  >
+    <button
+      v-if="y > 100"
+      type="button"
+      flex="~ items-center justify-center" of-hidden
+      class="size-8 bg-opacity-80 transition-all active:scale-105 hover:scale-115"
+    >
+      <i inline-block class="i-mingcute:up-line" />
+    </button>
+  </Transition>
+</template>
