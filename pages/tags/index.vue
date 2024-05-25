8c5821b60a27d8b5e7c45ce259d4865548e905f3
enpitsulin <enpitsulin@gmail.com>
2024-05-26 00:12:22 +0800

    feat: tag in tags page

--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -2,8 +2,38 @@
 useHead({
   title: 'Tags',
 })
+
+const { data } = useAsyncData(async () => {
+  const tags = await queryContent({
+    where: [
+      {
+        tags: {
+          $exists: true,
+        },
+      },
+    ],
+  }).only(['tags', 'title'])
+    .find()
+    .then((posts) => {
+      return posts.map(p => p.tags).flat()
+    })
+  const tagCountMap = tags.reduce((acc, curTag) => {
+    if (curTag in acc)
+      acc[curTag]++
+    else acc[curTag] = 1
+    return acc
+  }, {} as Record<string, number>)
+  return Object.entries(tagCountMap)
+})
 </script>
 
 <template>
-  <h1>tags</h1>
+  <div flex="~ wrap gap-2">
+    <Tag
+      v-for="([tag, count]) in data" :key="tag" :tag
+      flex="inline items-center gap-1"
+    >
+      {{ count }}
+    </Tag>
+  </div>
 </template>
