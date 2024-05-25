705ad63f9f4e4c944ca3508cd8d5e16a7ce8eba5
enpitsulin <enpitsulin@gmail.com>
2024-05-25 19:14:58 +0800

    fix: navbar link bg color

--- components/layout/NavBarLink.vue
+++ components/layout/NavBarLink.vue
@@ -10,7 +10,7 @@ defineProps<{
   <NuxtLink
     flex="~ items-center justify-center"
     h-full w-full cursor-pointer p-3
-    active-class=" active"
+    active-class="active"
     :href="href"
     class="navbar-link relative transition-color hover:text-gray-9 dark:hover:text-gray-6"
   >
@@ -20,7 +20,7 @@ defineProps<{
 
 <style>
 .navbar-link.active {
-  --at-apply: bg-white rounded-full;
+  --at-apply: bg-gray-100 rounded-full;
   view-transition-name: navbar-link;
 }
 
