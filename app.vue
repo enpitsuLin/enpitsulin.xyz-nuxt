353137768bac450f1cbdf6bc64fa23bd7e2ce1d9
enpitsulin <enpitsulin@gmail.com>
2024-05-27 16:57:53 +0800

    feat: app loader

--- app.vue
+++ app.vue
@@ -23,16 +23,19 @@ nuxtApp.hook('page:finish', () => {
 <template>
   <NuxtLoadingIndicator />
   <Transition
-    enter-active-class="transition-opacity duration-500 ease-out"
-    leave-active-class="transition-opacity duration-500 ease-out"
-    enter-from-class="op-0"
+    leave-active-class="transition-opacity duration-800"
     leave-to-class="op-0"
   >
     <div
       v-if="!loaded" fixed inset-0 z-99999 flex="~ items-center justify-center"
-      class="bg-[size:16px_16px] bg-slate-50 text-gray-950 dark:bg-black dark:text-gray-50"
+      class="bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-[size:16px_16px] bg-slate-50 text-gray-950 dark:bg-[radial-gradient(#e5e7eb20_1px,transparent_1px)] dark:bg-black dark:text-gray-50"
     >
-      loading...
+      <div flex="~ items-center justify-center" class="app-loader space-x-2 dark:invert">
+        <span class="sr-only">Loading...</span>
+        <div class="h-8 w-8 animate-bounce animate-delay-300 rounded-full bg-black" />
+        <div class="h-8 w-8 animate-bounce animate-delay-150 rounded-full bg-black" />
+        <div class="h-8 w-8 animate-bounce rounded-full bg-black" />
+      </div>
     </div>
   </Transition>
   <NuxtLayout>
