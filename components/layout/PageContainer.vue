abb21fe5fd0548dddecb557b7d6ae50bb41a502b
enpitsulin <enpitsulin@gmail.com>
2024-06-01 20:05:29 +0800

    feat: blog article search

--- components/layout/PageContainer.vue
+++ components/layout/PageContainer.vue
@@ -10,13 +10,14 @@ defineProps<{
     <header pb="10 md:20">
       <h2
         pb-10 font-bold text="4xl md:6xl" capitalize
-        class="animate-duration-1300 animate-ease-$spring-easing animate-in fade-in-0 slide-in-from-left-16"
+        class="animate-duration-800 animate-ease-$spring-easing animate-in fade-in-0 slide-in-from-left-16"
       >
         {{ title }}
       </h2>
-      <p class="animate-duration-1300 animate-delay-100 animate-ease-$spring-easing animate-in fade-in-0 slide-in-from-left-16">
+      <p class="animate-duration-800 animate-delay-100 animate-ease-$spring-easing animate-in fade-in-0 slide-in-from-left-16">
         {{ description }}
       </p>
+      <slot name="header" />
     </header>
     <slot />
   </div>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -50,6 +50,21 @@ async function loadMore() {
     title="全部文章"
     description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero neque recusandae atque earum a ut impedit dolor est temporibus animi repudiandae, similique fugit quisquam facilis autem, veniam doloribus hic reiciendis?"
   >
+    <template #header>
+      <div relative mt-4>
+        <input
+          b="~ border rounded-md"
+          h-10 w-full bg-transparent
+          flex="~" p="x-3 l-12 r-2" text-sm
+          class="ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-accent/50"
+          type="text"
+          placeholder="搜索文章"
+        >
+        <label class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" for="search">
+          <i class="i-mingcute:search-line absolute left-4 top-1/2 size-5 -translate-y-1/2" />
+        </label>
+      </div>
+    </template>
     <div pl="md:6" border="md:l border">
       <ul flex="~ col gap-16">
         <li v-for="article in data" :key="article._id">
