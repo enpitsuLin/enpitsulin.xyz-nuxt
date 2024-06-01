4e40f8d0dbade68d4c8afd6e9810e3a8bb9bbd8c
enpitsulin <enpitsulin@gmail.com>
2024-06-01 23:21:39 +0800

    fix: scrollbar-gutter

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,11 @@
 import { appDescription, appName } from '~/constants'
 import '~/styles/base.css'
 
+const { load } = useStyleTag(
+  `* { scrollbar-color: var(--c-scrollbar) transparent;}`,
+  { immediate: false },
+)
+
 useHead({
   titleTemplate(title) {
     if (title)
@@ -12,15 +17,21 @@ useHead({
     class: 'text-gray-950 dark:text-gray-50 bg-slate-50 dark:bg-black bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-[size:16px_16px] dark:bg-[radial-gradient(#e5e7eb20_1px,transparent_1px)]',
   },
 })
+
+onMounted(() => {
+  if (/macintosh|mac os x/i.test(navigator.userAgent)) {
+    load()
+  }
+})
 </script>
 
 <template>
-  <div px="sm:8" pointer-events-none fixed inset-0 flex="~ justify-center">
-    <div max-w-6xl w-full flex px="lg:8">
-      <div w-full class="bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+  <div relative px="sm:8" flex="~ 1 justify-center">
+    <div px="sm:8" pointer-events-none absolute inset-0 flex="~ justify-center">
+      <div max-w-6xl w-full flex px="lg:8">
+        <div w-full class="bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+      </div>
     </div>
-  </div>
-  <div px="sm:8" flex="~ 1 justify-center">
     <div
       flex="~ 1 col items-center"
       px="lg:8" relative max-w-6xl w-full
